bb35fb6d66f8e5a56557c4477def5ce799707d03
Myrone Silverio <silveriomyrone@gmail.com>
2024-11-08 19:12:12 +0800

    Add homepage components and structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image';
+import { Hero, Guide, Services, Offers, Events } from '@/components/home';
+import Newsletter from "@/components/newsletter";
 
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center w-svw h-svh">
-            Home
+            <Hero />
+            <Guide />
+            <Services />
+            <Offers />
+            <Events />
+            <Newsletter />
         </div>
     );
 }
---
+++ src/components/home/index.ts
@@ -0,0 +1,8 @@
+import Hero from "./hero";
+import Guide from "./guide";
+import Services from "./services";
+import Offers from "./offers";
+import Events from "./events";
+
+
+export { Hero, Guide, Services, Offers, Events}
