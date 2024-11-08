165c7675fb0ade9531b12fdc753ec7d78ba9ec67
Myrone Silverio <silveriomyrone@gmail.com>
2024-11-08 19:13:35 +0800

    Add Services section

---
+++ src/components/home/services.tsx
@@ -0,0 +1,11 @@
+export default function Services() {
+   return (
+      <div className="flex flex-col items-center text-center w-11/12 h-full my-8 space-y-5">
+         <h2 className="text-2xl font-semibold bg-[linear-gradient(90deg,rgba(255,209,0,1)0%,rgba(246,147,29,1)22%,rgba(0,160,75,1)78%,rgba(27,20,100,1)100%)] text-transparent bg-clip-text">BEST SERVICES FOR YOU!</h2>
+         <h3 className="mx-10">Try a Variety of Benefits When Using Our Services</h3>
+         <div className="relative w-40 h-60 rounded-[1.5rem] bg-[linear-gradient(0deg,rgba(255,209,0,1)12%,rgba(246,147,29,1)42%,rgba(0,160,75,1)78%,rgba(27,20,100,1)100%)]">
+         <div className="absolute flex items-center justify-center w-40 h-14 rounded-b-[1.5rem] bottom-0 bg-ginaYellow text-white font-bold">ACTIVITIES</div>
+         </div>
+      </div>
+   )
+}
