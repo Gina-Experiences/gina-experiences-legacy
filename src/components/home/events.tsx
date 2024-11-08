ecd03b4e502163b9c75ab65c536252587e13a5d7
Myrone Silverio <silveriomyrone@gmail.com>
2024-11-08 19:13:57 +0800

    Added Events section

---
+++ src/components/home/events.tsx
@@ -0,0 +1,11 @@
+export default function Events() {
+   return(
+      <div className="flex flex-col text-center my-6 mx-12 space-y-4">
+         <h2 className="">
+            Upcoming Events at <br></br>
+            <span className="text-3xl font-bold bg-[linear-gradient(90deg,rgba(255,209,0,1)0%,rgba(246,147,29,1)22%,rgba(0,160,75,1)78%,rgba(27,20,100,1)100%)] text-transparent bg-clip-text">Gina Experiences</span>
+         </h2>
+         <p className="text-sm">Stay in the loop with all the exciting happenings at <span className="font-bold">Gina Experiences!</span> From cultural festivals to adventure-packed excursions, there's always something exciting on the horizon. Don’t miss out on exclusive opportunities to explore, learn, and create lasting memories. Check back often to catch the latest updates and find your next adventure! <span className="font-bold">Tara na? G na!</span></p>
+      </div>
+   )
+}
