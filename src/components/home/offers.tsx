363acc57e434728b6c61b7f019c78b81061c04f8
Myrone Silverio <silveriomyrone@gmail.com>
2024-11-08 19:13:48 +0800

    Added Offers section

---
+++ src/components/home/offers.tsx
@@ -0,0 +1,9 @@
+export default function Offers() {
+   return (
+      <div className="flex flex-col justify-center items-center my-6 space-y-4">
+         <h2 className="text-2xl font-semibold bg-[linear-gradient(90deg,rgba(255,209,0,1)0%,rgba(246,147,29,1)22%,rgba(0,160,75,1)78%,rgba(27,20,100,1)100%)] text-transparent bg-clip-text">Best Offers This Month</h2>
+         <p className="text-sm mx-12">Discover the best travel deals and exclusive offers, tailored to make your dream vacation affordable and unforgettable.</p>
+         <button className="w-36 h-10 rounded-xl bg-ginaYellow text-white font-bold shadow-md">View More!</button>
+      </div>
+   )
+}
