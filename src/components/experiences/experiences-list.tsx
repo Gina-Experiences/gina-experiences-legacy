2f7b6c2ed34045a5d04506eefb6be92c323504f8
Luisa Eustaquio <95476379+xmuwisa@users.noreply.github.com>
2024-11-10 05:07:50 +0800

    Add Experiences, fix hero sections

---
+++ src/components/experiences/experiences-list.tsx
@@ -0,0 +1,7 @@
+export default function ExperiencesList() {
+    return (
+        <div className="w-full max-w-screen-2xl bg-ginaWhite h-auto p-24 mb-24 shadow-xl">
+            <div className="border border-red-700 w-full h-full"></div>
+        </div>
+    );
+}
