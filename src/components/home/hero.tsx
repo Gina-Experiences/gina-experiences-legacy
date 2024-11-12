1785ad34ab2bb9d5135d8e1b0a2157724a57d5e3
Myrone Silverio <silveriomyrone@gmail.com>
2024-11-12 11:59:28 +0800

    Fixed homepage hero banner, linked services and exp page

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,14 +1,50 @@
+'use client'
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { MapPin } from 'lucide-react';
+
+
 export default function Hero() {
-   return (
-    <div className="flex flex-col justify-center items-center w-11/12 max-w-screen-2xl h-full lg:h-[400px] text-white rounded-lg p-10 m-24 space-y-5 bg-gradient-to-b from-ginaOrange">
-        <div className=""> 
-            <span className="">Gina Experiences</span>
+    const App = () => {
+        return (
+            <MapPin />
+        );
+    };
+
+    const router = useRouter();
+
+    return (
+    <div className="flex flex-col items-center justify-center w-svw mb-28">
+            <div className="relative w-full max-w-screen-2xl h-svh p-8">
+                <div className="relative w-full h-full rounded-3xl overflow-hidden">
+                    <Image
+                        src="/images/home/gina-hero.png"
+                        alt="Hero"
+                        quality={100}
+                        width={1500}
+                        height={1500}
+                        className="rounded-3xl w-full h-full object-cover object-center"
+                    />
+                    <div className="absolute inset-0 bg-gradient-to-r from-ginaOrange via-transparent w-full h-full rounded-3xl"></div>
+                    <div className="absolute inset-0 w-full h-full flex flex-col items-start justify-center text-ginaWhite space-y-4 lg:space-y-16 p-8 md:p-24 ">
+                        <div className="flex flex-col space-y-3">
+                            <div className="flex space-x-2">
+                                <MapPin />
+                                <span className="font-normal text-base">Gina Experiences</span>
+                            </div>
+                            <h1 className="text-3xl lg:text-6xl font-bold">
+                                DISCOVER AUTHENTIC LOCAL EXPERIENCES
+                            </h1>
+                        </div>
+                        <span className="text-base lg:text-xl w-1/4">
+                        Immerse yourself in the local culture with unique experiences hosted by knowledgeable locals at <span className="font-bold">Gina Experiences.</span>
+                        </span>
+
+                        <button className="px-8 py-2 text-ginaOrange text-base font-bold bg-ginaWhite rounded-lg" onClick={() => router.push('/experiences')}>BOOK NOW!</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <h1 className="text-4xl font-bold">DISCOVER AUTHENTIC LOCAL EXPERIENCES</h1>
-        <p className="">
-        Immerse yourself in the local culture with unique experiences hosted by knowledgeable locals at <span className="font-bold">Gina Experiences</span>.
-        </p>
-        <button className="w-36 h-10 rounded-xl bg-white text-ginaOrange font-bold shadow-md">Book Now!</button>
-    </div>
    );
 }
