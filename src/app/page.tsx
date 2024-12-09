884d090d19f9d1fb534066cb84af9338a5b10142
Myrone Silverio <silveriomyrone@gmail.com>
2024-12-09 17:55:13 +0800

    Changed Newsletter component into pop-up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { Hero, Guide, Services, Offers, Events } from '@/components/home';
-import Newsletter from '@/components/newsletter';
+import Newsletter from '@/components/layout/newsletter-popup';
 
 export default function Home() {
     return (
@@ -10,7 +10,6 @@ export default function Home() {
             <Services />
             <Offers />
             <Events />
-            <Newsletter />
         </div>
     );
 }
