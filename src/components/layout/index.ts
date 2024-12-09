25a896a7954c13a40f1fcff97d00150a020b6161
Myrone Silverio <silveriomyrone@gmail.com>
2024-12-09 17:55:13 +0800

    Changed Newsletter component into pop-up

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,4 +1,5 @@
 import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
+import NewsletterBanner from "@/components/layout/newsletter-banner";
 
-export { Header, Footer };
+export { Header, NewsletterBanner, Footer };
--- src/components/layout/newsletter-popup.tsx
+++ src/components/layout/newsletter-popup.tsx
@@ -1,8 +1,13 @@
-import Image from "next/image"
+import * as Dialog from "@radix-ui/react-dialog"
+import { X } from 'lucide-react';
+import Form from 'next/form'
 
-export default function Newsletter() {
+export default function NewsletterPopup() {
    return(
-      <div className="flex rounded-3xl max-w-screen-lg lg:bg-ginaWhite lg:shadow-xl m-8">
+      <div className="flex lg:rounded-3xl w-screen max-w-screen-lg bg-ginaWhite lg:shadow-xl">
+         <Dialog.Close className="fixed right-0 m-8 cursor-pointer">
+            <X className="text-gray opacity-40 hover:opacity-100" />
+         </Dialog.Close>
          <div className="hidden w-2/3 lg:flex flex-col items-center p-12 space-y-2">
             <div className="flex h-2/3 space-x-2">
                <div className="flex flex-col w-1/2 h-full space-y-2">
@@ -29,9 +34,9 @@ export default function Newsletter() {
                <div className="w-1/3 h-full rounded-3xl bg-ginaGreen"></div>
             </div>
          </div>
-         <div className="flex flex-col lg:w-1/2 p-8 text-center items-center justify-center space-y-8">
-            <h2 className="text-2xl lg:text-3xl font-bold lg:text-end bg-[linear-gradient(90deg,rgba(255,209,0,1)0%,rgba(246,147,29,1)30%,rgba(0,160,75,1)78%,rgba(27,20,100,1)100%)] text-transparent bg-clip-text">Interested to <br />Find Out More?</h2>
-            <p className="text-sm font-medium lg:text-start">Sign up for our newsletter to get the latest updates on our experiences and promotions!</p>
+         <div className="flex flex-col w-screen lg:w-1/2 p-8 text-center items-center text-ginaBlack justify-center space-y-8">
+            <h2 className="text-3xl font-bold lg:text-end bg-[linear-gradient(90deg,rgba(255,209,0,1)0%,rgba(246,147,29,1)30%,rgba(0,160,75,1)78%,rgba(27,20,100,1)100%)] text-transparent bg-clip-text">Interested to <br />Find Out More?</h2>
+            <p className="text-sm font-medium lg:text-start px-8">Sign up for our newsletter to get the latest updates on our experiences and promotions!</p>
             <div className="space-y-5">
                <div className="flex space-x-2 lg:space-x-4">
                   <img src="images/newsletter/yellow-check.svg" 
@@ -55,10 +60,12 @@ export default function Newsletter() {
                   <span className="w-full flex text-xs justify-center items-center lg:justify-start">And much more!</span>
                </div>
             </div>
-            <div className="flex flex-col lg:flex-row items-center justify-center lg:space-x-4 space-y-5 lg:space-y-0">
-               <div className="flex p-2 px-4 lg:px-8 text-xs items-center border-2 border-ginaYellow rounded-lg text-ginaYellow ">Enter your email</div>
-               <button className="flex justify-center items-center rounded-xl bg-ginaYellow px-4 py-2 text-white text-sm font-medium shadow-md">Subscribe</button>
-            </div>
+            <Form action="/" className="flex flex-col lg:flex-row items-center justify-center lg:space-x-4 space-y-5 lg:space-y-0">
+               <input type="text" name="query" className="flex p-2 px-4 text-xs items-center text-ginaYellow border border-2 border-ginaYellow rounded-lg"/>
+               <Dialog.Close className="flex justify-center items-center rounded-xl bg-ginaYellow px-4 py-2 text-white text-sm font-medium hover:bg-ginaYellow/80 focus:ring focus:ring-ginaOrange/75 shadow-md">
+               Subscribe
+               </Dialog.Close>
+            </Form>
          </div>
       </div>
    )
