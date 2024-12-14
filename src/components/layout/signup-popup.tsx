90ce6059f3182e14bd5b23c25fec76f44cd32b10
Myrone Silverio <silveriomyrone@gmail.com>
2024-12-15 04:04:18 +0800

    Adjusted login and sign-up components

--- src/components/layout/signup-popup.tsx
+++ src/components/layout/signup-popup.tsx
@@ -1,17 +1,35 @@
 import Form from "next/form";
 import * as Dialog from "@radix-ui/react-dialog";
-import { X, EyeOff } from "lucide-react";
-import { useState } from "react";
+import { X, EyeOff, Eye } from "lucide-react";
+import React, { useState } from "react";
 
 const SignupPopup  = () => {
    const [firstName, setFirstName] = useState("First Name");
    const [lastName, setLastName] = useState("Last Name");
+   const [Birthdate, setBirthdate] = useState("Date of Birth");
+   const [gender, setGender] = useState("");
    const [contactNum, setContactNum] = useState("Contact Number");
    const [address, setAddress] = useState("Address");
    const [email, setEmail] = useState("Email");
    const [password, setPassword] = useState("Password");
-   const [confirmPassword, setConfirmPassword] = useState("Confirm Password");
+   const [confirmPassword, setConfirmPassword] = useState("Confirm Password")
+   const [error, setError] = useState('');
    
+   const [passwordVisible, setPasswordVisible] = useState(false);
+   const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+   const togglePasswordVisibility = () => setPasswordVisible(!passwordVisible);
+   const toggleConfirmPasswordVisibility = () => setConfirmPasswordVisible(!confirmPasswordVisible);
+   const [isAgreed, setIsAgreed] = useState(false);
+
+   const handleGenderChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setGender(e.target.value);
+      console.log("Selected Gender:", e.target.value);
+   };
+
+   const handleCheckboxChange = () => {
+      setIsAgreed(!isAgreed);
+   };
+
    const handleFocus = (
       field: string, 
       setValue: React.Dispatch<React.SetStateAction<string>>, 
@@ -40,8 +58,7 @@ const SignupPopup  = () => {
          <Dialog.Title></Dialog.Title>
          <Dialog.Portal>
             <Dialog.Overlay className="fixed inset-0 backdrop-blur-md"/>
-            <Dialog.Content className="fixed
-               top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50">
                <div className="flex lg:rounded-3xl w-screen max-w-screen-lg bg-ginaWhite lg:shadow-xl">
                   <Dialog.Close className="fixed right-0 m-8 cursor-pointer">
                      <X className="text-gray opacity-40 hover:opacity-100" />
@@ -73,69 +90,142 @@ const SignupPopup  = () => {
                      </div>
                   </div>
                   <div className="flex flex-col w-screen lg:w-1/2 p-8 text-center items-center text-ginaBlack justify-center space-y-8">
-                     <h2 className="text-4xl font-bold lg:text-end bg-[linear-gradient(90deg,rgba(255,209,0,1)0%,rgba(246,147,29,1)30%,rgba(0,160,75,1)78%,rgba(27,20,100,1)100%)] text-transparent bg-clip-text">Start Your Next <br />Adventure!</h2>
+                     <h2 className="text-4xl font-bold lg:text-end bg-[linear-gradient(90deg,rgba(255,209,0,1)0%,rgba(246,147,29,1)30%,rgba(0,160,75,1)78%,rgba(27,20,100,1)100%)] text-transparent bg-clip-text">
+                        Start Your Next <br />Adventure!
+                     </h2>
                      <p className="text-sm text-ginaBlack font-medium">Begin planning your dream getaway with ease.</p>
+                     
                      <Form action="/" className="flex flex-col items-center justify-center w-5/6 space-y-2 lg:space-x-0">
                         <div className="flex space-x-2">
-                           <input type="text" 
+                           <input
+                           type="text"
                            value={firstName}
                            onChange={(e) => setFirstName(e.target.value)}
                            onFocus={() => handleFocus(firstName, setFirstName, "First Name")}
                            onBlur={() => handleBlur(firstName, setFirstName, "First Name")}
-                           className="flex w-full p-2 px-4 text-xs items-center text-ginaYellow border border-2 border-ginaYellow rounded-lg"/>
-                           <input type="text" 
+                           className="flex w-full p-2 px-4 text-xs items-center text-ginaBlue border border-2 border-ginaBlue rounded-lg"
+                           required
+                           />
+                           <input
+                           type="text"
                            value={lastName}
                            onChange={(e) => setLastName(e.target.value)}
                            onFocus={() => handleFocus(lastName, setLastName, "Last Name")}
                            onBlur={() => handleBlur(lastName, setLastName, "Last Name")}
-                           className="flex w-full p-2 px-4 text-xs items-center text-ginaYellow border border-2 border-ginaYellow rounded-lg"/>
+                           className="flex w-full p-2 px-4 text-xs items-center text-ginaBlue border border-2 border-ginaBlue rounded-lg"
+                           required
+                           />
                         </div>
-                        <input type="text" 
-                        value={contactNum}
-                        onChange={(e) => setContactNum(e.target.value)}
-                        onFocus={() => handleFocus(contactNum, setContactNum, "Contact Number")}
-                        onBlur={() => handleBlur(contactNum, setContactNum, "Contact Number")}
-                        className="flex w-full p-2 px-4 text-xs items-center text-ginaYellow border border-2 border-ginaYellow rounded-lg"/>
-                        <input type="text" 
-                        value={address}
-                        onChange={(e) => setAddress(e.target.value)}
-                        onFocus={() => handleFocus(address, setAddress, "Address")}
-                        onBlur={() => handleBlur(address, setAddress, "Address")}
-                        className="flex w-full p-2 px-4 text-xs items-center text-ginaYellow border border-2 border-ginaYellow rounded-lg"/>
-                        <input type="text" 
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        onFocus={() => handleFocus(email, setEmail, "Email")}
-                        onBlur={() => handleBlur(email, setEmail, "Email")}
-                        className="flex w-full p-2 px-4 text-xs items-center text-ginaYellow border border-2 border-ginaYellow rounded-lg"/>
+                        
+                        <div className="flex space-x-2">
+                           <input
+                           type="text"
+                           value={Birthdate}
+                           onChange={(e) => setBirthdate(e.target.value)}
+                           onFocus={() => handleFocus(Birthdate, setBirthdate, "Date of Birth")}
+                           onBlur={() => handleBlur(Birthdate, setBirthdate, "Date of Birth")}
+                           className="flex w-1/2 p-2 px-4 text-xs items-center text-ginaBlue border border-2 border-ginaBlue rounded-lg"
+                           required
+                           />
+                           <select
+                           id="gender"
+                           value={gender}
+                           onChange={handleGenderChange}
+                           className="w-1/2 p-2 px-4 text-xs items-center text-ginaBlue border border-2 border-ginaBlue rounded-lg"
+                           required
+                           >
+                           <option value="" disabled>Gender</option>
+                           <option value="male">Male</option>
+                           <option value="female">Female</option>
+                           <option value="other">Other</option>
+                           <option value="preferNotToSay">Prefer Not to Say</option>
+                           </select>
+                        </div>
+                        
+                        <input
+                           type="text"
+                           value={contactNum}
+                           onChange={(e) => setContactNum(e.target.value)}
+                           onFocus={() => handleFocus(contactNum, setContactNum, "Contact Number")}
+                           onBlur={() => handleBlur(contactNum, setContactNum, "Contact Number")}
+                           className="flex w-full p-2 px-4 text-xs items-center text-ginaBlue border border-2 border-ginaBlue rounded-lg"
+                           required
+                        />
+                        <input
+                           type="text"
+                           value={address}
+                           onChange={(e) => setAddress(e.target.value)}
+                           onFocus={() => handleFocus(address, setAddress, "Address")}
+                           onBlur={() => handleBlur(address, setAddress, "Address")}
+                           className="flex w-full p-2 px-4 text-xs items-center text-ginaBlue border border-2 border-ginaBlue rounded-lg"
+                           required
+                        />
+                        <input
+                           type="text"
+                           value={email}
+                           onChange={(e) => setEmail(e.target.value)}
+                           onFocus={() => handleFocus(email, setEmail, "Email")}
+                           onBlur={() => handleBlur(email, setEmail, "Email")}
+                           className="flex w-full p-2 px-4 text-xs items-center text-ginaBlue border border-2 border-ginaBlue rounded-lg"
+                           required
+                        />
+                        
                         <div className="relative w-full max-w-md">
-                           <input type="text"
+                           <input
+                           type={passwordVisible ? "text" : "password"}
                            value={password}
                            onChange={(e) => setPassword(e.target.value)}
                            onFocus={() => handleFocus(password, setPassword, "Password")}
                            onBlur={() => handleBlur(password, setPassword, "Password")}
-                           className="flex w-full p-2 px-4 text-xs items-center text-ginaYellow border border-2 border-ginaYellow rounded-lg"/>
-                           {/* <EyeOff /> */}
+                           className="flex w-full p-2 px-4 text-xs items-center text-ginaBlue border border-2 border-ginaBlue rounded-lg"
+                           required
+                           />
+                           <div 
+                              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer" 
+                              onClick={togglePasswordVisibility}
+                           >
+                           {passwordVisible ? <Eye size={20} /> : <EyeOff size={20} />}
+                           </div>
                         </div>
+                        
                         <div className="relative w-full max-w-md">
-                           <input type="text"
+                           <input
+                           type={confirmPasswordVisible ? "text" : "password"}
                            value={confirmPassword}
                            onChange={(e) => setConfirmPassword(e.target.value)}
                            onFocus={() => handleFocus(confirmPassword, setConfirmPassword, "Confirm Password")}
                            onBlur={() => handleBlur(confirmPassword, setConfirmPassword, "Confirm Password")}
-                           className="flex w-full p-2 px-4 text-xs items-center text-ginaYellow border border-2 border-ginaYellow rounded-lg"/>
-                           {/* <EyeOff /> */}
+                           className="flex w-full p-2 px-4 text-xs items-center text-ginaBlue border border-2 border-ginaBlue rounded-lg"
+                           required
+                           />
+                           <div 
+                              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer" 
+                              onClick={toggleConfirmPasswordVisibility}
+                           >
+                           {confirmPasswordVisible ? <Eye size={20} /> : <EyeOff size={20} />}
+                           </div>             
                         </div>
                      </Form>
-                     <div className="flex space-x-4">
-                        <Dialog.Close className="flex justify-center items-center rounded-xl px-8 py-2 text-sm text-ginaYellow  font-medium border border-2 border-ginaYellow hover:bg-ginaGray focus:ring focus:ring-ginaOrange/50 shadow-md">
-                        Sign Up
-                        </Dialog.Close>
-                        <Dialog.Close className="flex justify-center items-center rounded-xl bg-ginaYellow px-10 py-2 text-white text-sm font-medium hover:bg-ginaYellow/80 focus:ring focus:ring-ginaOrange/50 shadow-md">
-                        Login
-                        </Dialog.Close>
+                     
+                     <div className="flex gap-4">
+                        <input
+                           type="checkbox"
+                           checked={isAgreed}
+                           onChange={handleCheckboxChange}
+                           required
+                        />
+                        <span className="text-xs">Agree to the terms and conditions</span>
                      </div>
-                        <p className="text-ginaYellow text-sm cursor-pointer">Already have an account? Login here!</p>
+                     
+                     {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+
+                     <button 
+                        type="submit"
+                        className="flex justify-center items-center rounded-xl px-8 py-2 text-sm text-ginaWhite bg-ginaBlue font-medium border border-2 border-ginaBlue hover:bg-ginaWhite hover:text-ginaBlue focus:ring focus:ring-ginaBlue/50 shadow-md">
+                        Sign Up
+                     </button>
+                     
+                     <p className="text-ginaBlue text-sm cursor-pointer">Already have an account? Login here!</p>
                   </div>
                </div>
             </Dialog.Content>
