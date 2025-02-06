456861088dcb9a5b86497f8f5d4f74a0a7c3f2ec
Luisa Eustaquio <95476379+xmuwisa@users.noreply.github.com>
2025-02-07 07:14:44 +0800

    fix ui

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -9,9 +9,9 @@ export default function ProtectedLayout({
     return (
         <div className="w-full flex items-center justify-center">
             <div className="w-full h-svh p-0 lg:p-8">
-                <div className="w-full h-full rounded-none lg:rounded-[50px] bg-gradient-to-r from-ginaYellow to-ginaOrange flex p-4 lg:p-8 gap-4 lg:gap-8">
+                <div className="w-full h-full rounded-none lg:rounded-[50px] bg-gradient-to-r from-ginaYellow to-ginaOrange flex p-4 lg:p-8 gap-4 lg:gap-8 ">
                     <Sidebar />
-                    <div className="w-full h-full border-2 bg-ginaWhite rounded-3xl lg:rounded-[34px] p-8 lg:pt-8 pt-20">
+                    <div className="w-full h-full border-2 bg-ginaWhite rounded-3xl lg:rounded-[34px] p-8 lg:pt-8 pt-20 overflow-y-auto">
                         {children}
                     </div>
                 </div>
